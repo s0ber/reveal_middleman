50d6d169b8a9d8c9e0ee7916711355a842e3ca30
Sergey <sergeyshishkalov@gmail.com>
2013-05-08 06:39:43 +0400

    some little fixes

--- helpers/site_helpers.rb
+++ helpers/site_helpers.rb
@@ -1,13 +1,13 @@
 module SiteHelpers
 
   def page_title
-    title = "Set your site title in /helpers/site_helpers.rb"
+    title = "Reveal.js"
     if data.page.title
       title << " | " + data.page.title
     end
     title
   end
-  
+
   def page_description
     if data.page.description
       description = data.page.description
